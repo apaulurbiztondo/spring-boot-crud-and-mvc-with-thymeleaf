3bed17311662a099d6bc43e7b8fddb25a18010d6
allan <allanpaulourbiztondo@gmail.com>
2023-06-05 02:46:08 +0800

    feat: logout and improve UI

--- src/main/java/com/springboot/thymeleafdemo/controller/DemoController.java
+++ src/main/java/com/springboot/thymeleafdemo/controller/DemoController.java
@@ -9,12 +9,12 @@ public class DemoController {
 
 	// create a mapping for "/hello"
 	
-	@GetMapping("/hello")
+	@GetMapping("/")
 	public String sayHello(Model theModel) {
 		
 		theModel.addAttribute("theDate", new java.util.Date());
 		
-		return "helloworld";
+		return "dashboard";
 	}
 }
 
